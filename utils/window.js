c2d3059f90d63f72373f8fc6dd75f7aae7b0958d
Jaycen <saitakusei@gmail.com>
2025-07-06 17:35:09 +0900

    Open search bar in differnet montior

--- utils/window.js
+++ utils/window.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { BrowserWindow } from 'electron';
+import { BrowserWindow, screen } from 'electron';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -32,6 +32,23 @@ export const openWindow = (win) => {
   if (win.isVisible()) {
     win.hide();
   } else {
+    const mousePoint = screen.getCursorScreenPoint();
+    const display = screen.getDisplayNearestPoint(mousePoint);
+    const { x, y, width, height } = display.workArea;
+
+    const windowWidth = 600;
+    const windowHeight = win.getBounds().height;
+
+    const centerX = x + Math.round((width - windowWidth) / 2);
+    const centerY = y + Math.round((height - windowHeight) / 2);
+
+    win.setBounds({
+      x: centerX,
+      y: centerY,
+      width: windowWidth,
+      height: windowHeight
+    });
+
     win.show();
     win.focus();
     win.webContents.send('reset');
