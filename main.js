eb9ac440aaae00b67c9f35a626b88ebc886c3a5d
Jaycen <saitakusei@gmail.com>
2025-07-06 13:54:48 +0900

    Add search func

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ let win;
 
 app.whenReady().then(() => {
   win = new BrowserWindow({
+    center: true,
     width: 600,
     height: 60,
     frame: false,
@@ -19,13 +20,15 @@ app.whenReady().then(() => {
     resizable: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-    },
+      contextIsolation: true,
+      nodeIntegration: false,
+    }
   });
 
   win.loadFile('index.html');
   win.hide();
 
-  globalShortcut.register('Super+Alt', () => {
+  globalShortcut.register('Ctrl+Alt+S', () => {
     if (win.isVisible()) {
       win.hide();
     } else {
@@ -35,10 +38,18 @@ app.whenReady().then(() => {
   });
 
   ipcMain.on('search', async (_, query) => {
+    console.log('Search received in main:', query);
     const url = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
     await open(url);
     win.hide();
   });
+
+  ipcMain.on('hide-window', () => {
+  if (win) {
+    win.hide();
+  }
+});
+
 });
 
 app.on('will-quit', () => {
--- preload.js
+++ preload.js
@@ -2,4 +2,5 @@ import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('electronAPI', {
   search: (query) => ipcRenderer.send('search', query),
+  hideWindow: () => ipcRenderer.send('hide-window'),
 });
