32729da6530a03599d5941e8e398f6ddf744af30
Jaycen <saitakusei@gmail.com>
2025-07-06 14:48:09 +0900

    Add suggestions

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 import { app, globalShortcut, BrowserWindow, ipcMain } from 'electron';
-import { Tray, Menu, nativeImage } from 'electron';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import open from 'open';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-console.log('Current directory:', __dirname);
 let win;
 
 app.whenReady().then(() => {
@@ -29,8 +27,6 @@ app.whenReady().then(() => {
   });
 
   win.loadFile('index.html');
-  win.hide();
-  // win.webContents.openDevTools();
 
   globalShortcut.register('Alt+Space', () => {
     if (win.isVisible()) {
@@ -42,8 +38,6 @@ app.whenReady().then(() => {
   });
 
   ipcMain.on('search', async (_, query) => {
-    console.log('Search received in main:', query);
-
     const [cmd, ...args] = query.trim().split(/\s+/);
     const q = encodeURIComponent(args.join(' '));
 
@@ -52,7 +46,6 @@ app.whenReady().then(() => {
       gh: `https://github.com/search?q=${q}`,
       g: `https://www.google.com/search?q=${q}`,
       wiki: `https://en.wikipedia.org/wiki/${q}`,
-      // fallback if only one word
       default: `https://www.google.com/search?q=${encodeURIComponent(query)}`
     };
 
@@ -62,26 +55,15 @@ app.whenReady().then(() => {
   });
 
   ipcMain.on('hide-window', () => {
-  if (win) {
-    win.hide();
-  }
-});
+    if (win) win.hide();
+  });
 
-});
-
-app.whenReady().then(() => {
-  // Load icon (must be .ico or .png)
-  const iconPath = path.join(__dirname, 'icon.png');
-  const trayIcon = nativeImage.createFromPath(iconPath);
-  tray = new Tray(trayIcon);
-
-  const contextMenu = Menu.buildFromTemplate([
-    { label: 'Show Search', click: () => win.show() },
-    { label: 'Quit', click: () => app.quit() }
-  ]);
-
-  tray.setToolTip('Spotlight Launcher');
-  tray.setContextMenu(contextMenu);
+  ipcMain.on('resize-window', (_, height) => {
+    if (win) {
+      const bounds = win.getBounds();
+      win.setBounds({ x: bounds.x, y: bounds.y, width: 600, height });
+    }
+  });
 });
 
 app.on('will-quit', () => {
--- preload.js
+++ preload.js
@@ -3,4 +3,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   search: (query) => ipcRenderer.send('search', query),
   hideWindow: () => ipcRenderer.send('hide-window'),
+  resizeWindow: (height) => ipcRenderer.send('resize-window', height)
 });
