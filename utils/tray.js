d9d12488240b96162f15467b21d53a969b6935a5
Jaycen <saitakusei@gmail.com>
2025-07-08 17:00:38 +0900

    Fix tray cannot open issue

--- utils/tray.js
+++ utils/tray.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { app, Tray, Menu } from 'electron';
+import { openWindow } from './window.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,7 +10,7 @@ export const setUpTray = (win) => {
   const tray = new Tray(path.join(__dirname, '..' ,'assets', 'icon.png')); 
   const contextMenu = Menu.buildFromTemplate([
     {
-      label: 'Toggle Search (Alt+Space)',
+      label: 'Toggle Search (Ctrl+Space)',
       click: () => {
         openWindow(win);
       }
